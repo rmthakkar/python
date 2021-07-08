5fd70e3c8b7ae89612c96ed52780b40b6c4f7402
rmthakkar <riteshrt@gmail.com>
2021-07-08 10:50:16 +1000

    done

---
+++ venv/HelloWorld.py
@@ -0,0 +1,31 @@
+print('hello world')
+print('ritesh')
+a = 10
+print(a)
+b = a + 100
+print("the value of b is " ,b )
+a =20
+b =50
+c = a*b
+print(" this is the multiplication ", c)
+
+if c > 100:
+    print("if condition satisified")
+else:
+    print("no")
+
+#this is the comment and not going to execute
+c="Ritesh Thakkar"
+print(c)
+print(type(c))
+print(len(c))
+print(c[1:14])
+d = c[1:14]
+print(d.upper() + "cat")
+mylist = [10,20,30,40,50]
+print(mylist)
+mylist.append(60)
+print(mylist)
+mylist.insert(3,35)
+print(mylist)
+print(mylist[2])
